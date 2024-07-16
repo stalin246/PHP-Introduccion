a4beac6be4c968c86db3f90f1ad71473e41b41a4
Luis Valencia <stalinvalencia24@gmail.com>
2024-07-16 12:00:29 -0500

    update

--- includes/header.php
+++ includes/header.php
@@ -13,7 +13,7 @@
 
     <div class="contenido">
         <h1>Desarrollo Web Completo</h1>
-        <a href="http://www.codigoconjuan.com" target="_blank">codigoconjuan.com</a>
+
         <p>Introducción a PHP</p>
         <p class="hashtag">#DesarrolloWebCompleto</p>
 
